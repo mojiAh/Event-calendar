7558c6752322948939001e6dde0cee94d3f5a4ba
Moji <moji@MacBook-Pro.home>
2017-05-15 19:55:11 +0300

    add basic authentication Application files

---
+++ app/src/main/java/com/booking/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.booking.utils;
+
+public class Constants {
+    public static final String BASE_URL = "https://mobileauthentication.herokuapp.com/api/v1/";
+    public static final String TOKEN = "token";
+    public static final String EMAIL = "email";
+}
